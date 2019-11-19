c577492aa754fd52f764bfc9de13bf6e0b07438b
Oleh Momot <Oleh_Momot@pm.me>
2019-11-19 14:12:51 +0200

    Create pre process response service

--- registry/server/routes/apps/getApps.ts
+++ registry/server/routes/apps/getApps.ts
@@ -5,11 +5,12 @@ import {
 
 import db from '../../db';
 import App from '../../apps/interfaces/App';
+import preProcessResponse from '../../services/preProcessResponse';
 
 const getApps = async (req: Request, res: Response) => {
   const apps = await db.select().from<App>('apps');
 
-  return res.status(200).send(apps);
+  return res.status(200).send(preProcessResponse(apps));
 };
 
 export default getApps;
---
+++ registry/server/services/preProcessResponse.ts
@@ -0,0 +1,47 @@
+import _ from 'lodash/fp';
+
+/**
+ * Original source code was taken from {@link https://github.com/prototypejs/prototype/blob/5fddd3e/src/prototype/lang/string.js#L702}
+ */
+const isJSON = (str: string): boolean => {
+  if (/^\s*$/.test(str)) return false;
+
+  str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@');
+  str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
+  str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
+
+  return (/^[\],:{}\s]*$/).test(str);
+};
+
+const parseJSON = (value: any): any => {
+  if (_.isString(value) && isJSON(value)) {
+      return JSON.parse(value);
+  }
+
+  return value;
+}
+
+const omitEmptyValues = _.omitBy(_.cond([
+  [_.isNull, _.stubTrue],
+  [_.isEmpty, _.stubTrue],
+  [_.stubTrue, _.stubFalse]
+]));
+
+const omitPreResponseEmptyValues = _.cond([
+  [_.isArray, _.map(omitEmptyValues)],
+  [_.isObject, omitEmptyValues],
+  [_.stubTrue, (value: any) => value]
+]);
+
+const parsePreResponseJSON = _.cond([
+  [_.isArray, _.map(_.mapValues(parseJSON))],
+  [_.isObject, _.mapValues(parseJSON)],
+  [_.stubTrue, parseJSON]
+]);
+
+const preProcessResponse = _.compose(
+  omitPreResponseEmptyValues,
+  parsePreResponseJSON,
+);
+
+export default preProcessResponse;
