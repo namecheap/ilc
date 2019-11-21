ad5a367241256febd0bf27cb52dc700573be8984
Oleh Momot <Oleh_Momot@pm.me>
2019-11-21 12:51:27 +0200

    Move validate request to services folder, update app`s routes

--- registry/server/apps/routes/updateApp.ts
+++ registry/server/apps/routes/updateApp.ts
@@ -6,26 +6,45 @@ import Joi from '@hapi/joi';
 import _ from 'lodash/fp';
 
 import db from '../../db';
-import validationMiddleware, {
+import validateRequest, {
     ValidationPairs,
     selectBodyToValidate,
     selectQueryToValidate
-} from '../../common/middlewares/validationMiddleware';
+} from '../../common/services/validateRequest';
 import preProcessResponse from '../../common/services/preProcessResponse';
-import { prepareAppToInsert } from '../services/prepareAppsToInsert';
-import App, { AppBody, AppName, appNameSchema, partialAppBodySchema } from '../interfaces/App';
+import {
+    prepareAppToInsert,
+} from '../services/prepareAppsToInsert';
+import App, {
+    AppBody,
+    AppName,
+    appNameSchema,
+    partialAppBodySchema,
+} from '../interfaces/App';
 
 type UpdateAppRequestBody = AppBody;
 type UpdateAppRequestQuery = {
     name: AppName
 };
 
+const updateAppRequestQuery = Joi.object({
+    name: appNameSchema.required(),
+});
+const updateAppValidationPairs: ValidationPairs = new Map([
+    [updateAppRequestQuery, selectQueryToValidate],
+    [partialAppBodySchema, selectBodyToValidate],
+]);
+
+const validateAppBeforeUpdate = validateRequest(updateAppValidationPairs);
+
 const selectAppDataToUpdate = (app: AppBody): Partial<App> => _.compose<any, App, Partial<App>>(
     _.pick(_.keys(app)),
     prepareAppToInsert,
 )(app);
 
-const updateApp = async (req: Request, res: Response) => {
+const updateApp = async (req: Request, res: Response): Promise<void> => {
+    await validateAppBeforeUpdate(req, res);
+
     const app: UpdateAppRequestBody = req.body;
     const {
         name: appName
@@ -37,17 +56,7 @@ const updateApp = async (req: Request, res: Response) => {
     const updatedAppName = appDataToUpdate.name || appName;
     const [updatedApp]: Array<App> = await db.select().from<App>('apps').where('name', updatedAppName);
 
-    return res.status(200).send(preProcessResponse(updatedApp));
+    res.status(200).send(preProcessResponse(updatedApp));
 };
-
-const updateAppRequestQuery = Joi.object({
-    name: appNameSchema.required(),
-});
-const updateAppValidationPairs: ValidationPairs = new Map([
-    [updateAppRequestQuery, selectQueryToValidate],
-    [partialAppBodySchema, selectBodyToValidate],
-]);
-
-export const validateAppBeforeUpdate = validationMiddleware(updateAppValidationPairs);
 
 export default updateApp;
