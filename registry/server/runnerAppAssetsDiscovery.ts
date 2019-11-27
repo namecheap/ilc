fc2bd157ac4660b7ee3ef9a6deafb483d7bbeba7
Volodymyr Makukha <nei.css@gmail.com>
2019-11-27 16:36:27 +0200

    chore: interval to upper

--- registry/server/runnerAppAssetsDiscovery.ts
+++ registry/server/runnerAppAssetsDiscovery.ts
@@ -1,4 +1,4 @@
-const tmpInterval = Number(process.env.interval);
+const tmpInterval = Number(process.env.INTERVAL);
 const interval = Number.isNaN(tmpInterval) ? undefined : tmpInterval;
 
 import AppAssetsDiscovery from './services/AppAssetsDiscovery';
