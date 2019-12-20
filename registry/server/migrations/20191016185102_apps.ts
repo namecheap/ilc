ae53b479373965a93d9355981cacdf1b2d744f2c
Vladlen Fedosov <vladlen.f@namecheap.com>
2019-12-20 15:01:47 +0200

    Migration fix

--- registry/server/migrations/20191016185102_apps.ts
+++ registry/server/migrations/20191016185102_apps.ts
@@ -3,7 +3,7 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<any> {
     return knex.schema.createTable('apps', table => {
-        table.string('name', 50).notNullable().unique();
+        table.string('name', 50).notNullable().primary();
         table.string('spaBundle', 255).notNullable();
         table.string('cssBundle', 255).nullable();
         table.json('dependencies').defaultTo('{}');
