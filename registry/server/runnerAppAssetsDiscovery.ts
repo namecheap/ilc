68219b9c3fed0851d3f82505884990ea5e3ff868
Vladlen Fedosov <vladlen.f@namecheap.com>
2019-12-25 14:42:32 +0200

    NewRelic added to runnerAppAssetsDiscovery

--- registry/server/runnerAppAssetsDiscovery.ts
+++ registry/server/runnerAppAssetsDiscovery.ts
@@ -1,3 +1,8 @@
+import path from "path";
+
+process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../config');
+require('newrelic'); //Should be lower then NODE_CONFIG_DIR env var definition
+
 const tmpInterval = Number(process.env.INTERVAL);
 const interval = Number.isNaN(tmpInterval) ? undefined : tmpInterval;
 
