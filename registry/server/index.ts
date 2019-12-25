4f618b338f8c9c89f25b174c4c5be68fcaad1659
Vladlen Fedosov <vladlen.f@namecheap.com>
2019-12-25 13:19:49 +0200

    NewRelic basic integration was added to Registry

--- registry/server/index.ts
+++ registry/server/index.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../config');
-// require('newrelic'); //Should be lower then NODE_CONFIG_DIR env var definition
+require('newrelic'); //Should be lower then NODE_CONFIG_DIR env var definition
 
 import app from './app';
 import server from './server';
