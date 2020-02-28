49ff77959a113ef4134f8305af0ed8745b4e7567
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-02-28 15:58:47 +0200

    feat: additional NewRelic instrumentation

--- ilc/client/performance.js
+++ ilc/client/performance.js
@@ -1,9 +1,16 @@
 
 export default function init(getCurrentPath) {
     let startRouting;
+    let nrInteraction = null;
+    let nrTrace = null;
 
     window.addEventListener('single-spa:before-routing-event', () => {
         startRouting = performance.now();
+
+        if (newrelic && newrelic.interaction) {
+            nrInteraction = newrelic.interaction();
+            nrTrace = nrInteraction.createTracer('routeChange');
+        }
     });
 
     window.addEventListener('ilc:all-slots-loaded', () => {
@@ -15,6 +22,13 @@ export default function init(getCurrentPath) {
 
         if (newrelic && newrelic.addPageAction) {
             newrelic.addPageAction('routeChange', { time: timeMs, route: currentPath.route })
+        }
+
+        if (nrTrace) {
+            nrTrace();
+        }
+        if (nrInteraction) {
+            nrInteraction.end();
         }
     });
 }
