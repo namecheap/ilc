b7a4a53b0602faf55856fdd63f2d2df7e637df5c
Oleh Momot <oleh_momot@pm.me>
2020-03-25 18:23:56 +0200

    Remove FOUC fix

--- ilc/server/tailor/configs-injector.js
+++ ilc/server/tailor/configs-injector.js
@@ -24,7 +24,6 @@ module.exports = class ConfigsInjector {
         const regConf = registryConf.data;
 
         document = document.replace('</body>', this.#wrapWithIgnoreDuringParsing(
-            this.#hideHTMLtoAvoidFOUC(false),
             this.#getSPAConfig(regConf),
             this.#getPolyfill(),
             this.#wrapWithAsyncScriptTag(this.#getClientjsUrl()),
@@ -32,7 +31,6 @@ module.exports = class ConfigsInjector {
 
         document = document.replace('</head>', this.#wrapWithIgnoreDuringParsing(
             newrelic.getBrowserTimingHeader(),
-            this.#hideHTMLtoAvoidFOUC(),
         ) + '</head>');
 
         const routeAssets = await this.#getRouteAssets(reqUrl);
@@ -121,21 +119,6 @@ module.exports = class ConfigsInjector {
 
         return `<script type="spa-config">${JSON.stringify(_.omit(registryConf, ['templates']))}</script>`;
     };
-
-    /**
-     * This style is needed to avoid a flash of unstyled content (FOUC) on Firefox
-     * 
-     * @see {@link https://bugzilla.mozilla.org/show_bug.cgi?id=1404468}
-     * @see {@link https://petrey.co/2017/05/the-most-effective-way-to-avoid-the-fouc/}
-     * @see {@link https://gist.github.com/electrotype/7960ddcc44bc4aea07a35603d1c41cb0#file-fouc-fix-md}
-     * @see {@link https://stackoverflow.com/questions/952861/targeting-only-firefox-with-css}
-     */
-    #hideHTMLtoAvoidFOUC = (hideHTML = true) => {
-        const visibility = hideHTML ? 'hidden' : 'visible';
-        const opacity = hideHTML ? 0 : 1;
-
-        return `<style>@supports (-moz-appearance:none) { html { visibility: ${visibility}; opacity: ${opacity}; }}</style>`;
-    }
 
     #wrapWithAsyncScriptTag = (url) => {
         return `<script src="${url}" type="text/javascript" ${this.#getCrossoriginAttribute(url)} async></script>`;
