a92654be4eb93a59ecb9e4f93fb43800bb189d0a
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-03-30 19:10:22 +0300

    bugfix of the systemjs-override-importmap.js

--- ilc/systemjs/systemjs-override-importmap.js
+++ ilc/systemjs/systemjs-override-importmap.js
@@ -7,18 +7,18 @@
 
     const existingResolve = systemJSPrototype.resolve;
     systemJSPrototype.resolve = function (id, parentUrl) {
-        if (resourcesMap[id]) {
-            return resourcesMap[id];
-        }
+        try {
+            return existingResolve.call(this, id, parentUrl);
+        } catch (e) { // We override only dependencies that weren't declared previously
+            if (resourcesMap[id]) {
+                return resourcesMap[id];
+            }
 
-        return existingResolve.call(this, id, parentUrl);
+            throw e;
+        }
     };
 
     systemJSPrototype.overrideImportMap = function (id, url) {
-        try {
-            existingResolve.call(this, id);
-        } catch (e) { // We override only dependencies that weren't declared previously
-            resourcesMap[id] = url;
-        }
+        resourcesMap[id] = url;
     }
 })();
