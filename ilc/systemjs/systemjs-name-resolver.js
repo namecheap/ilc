90ea774304bbbb004bca822e91ac951ff23d41e6
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-03-30 17:25:10 +0300

    SystemJS resolve now is implemented as a synchronous function according to spec. https://github.com/systemjs/systemjs/blob/master/docs/hooks.md#resolveid-parenturl---string

--- ilc/systemjs/systemjs-name-resolver.js
+++ ilc/systemjs/systemjs-name-resolver.js
@@ -7,27 +7,25 @@
 
     const existingResolve = systemJSPrototype.resolve;
     systemJSPrototype.resolve = function (id, parentUrl) {
-        return Promise.resolve(existingResolve.call(this, id, parentUrl))
-            .then(function (existingHookResult) {
-                if (id === existingHookResult) { //unnamed module
-                    return existingHookResult;
-                }
+        const existingHookResult = existingResolve.call(this, id, parentUrl);
+        if (id === existingHookResult) { //unnamed module
+            return existingHookResult;
+        }
 
-                if (resourcesMap[id] === undefined) {
-                    resourcesMap[id] = {
-                        name: id,
-                        src: existingHookResult,
-                        dependants: []
-                    }
-                }
+        if (resourcesMap[id] === undefined) {
+            resourcesMap[id] = {
+                name: id,
+                src: existingHookResult,
+                dependants: []
+            }
+        }
 
-                if (parentUrl !== undefined && !resourcesMap[id].dependants.includes(parentUrl)) {
-                    resourcesMap[id].dependants.push(parentUrl);
-                }
+        if (parentUrl !== undefined && !resourcesMap[id].dependants.includes(parentUrl)) {
+            resourcesMap[id].dependants.push(parentUrl);
+        }
 
-                // custom hook here
-                return existingHookResult;
-            });
+        // custom hook here
+        return existingHookResult;
     };
 
     systemJSPrototype.getModuleInfo = function (id) {
