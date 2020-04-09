57d2b3b7a29d5d7b4c20badccde9938a38e0c95e
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-04-09 18:46:32 +0300

    chore: tests fix

--- registry/server/app.ts
+++ registry/server/app.ts
@@ -10,30 +10,34 @@ import errorHandler from './errorHandler';
 import serveStatic from 'serve-static';
 import auth from './auth';
 
-// As in production there can be 2+ instances of the ILC registry
-// AppAssetsDiscovery should be run separately via "npm run assetsdiscovery"
-!['production', 'test'].includes(process.env.NODE_ENV!) && require('./runnerAppAssetsDiscovery');
+export default (withAuth: boolean = true) => {
+    // As in production there can be 2+ instances of the ILC registry
+    // AppAssetsDiscovery should be run separately via "npm run assetsdiscovery"
+    !['production', 'test'].includes(process.env.NODE_ENV!) && require('./runnerAppAssetsDiscovery');
 
-const app = express();
+    const app = express();
 
-app.use(bodyParser.json());
+    app.use(bodyParser.json());
 
-app.get('/ping', pong);
+    app.get('/ping', pong);
 
-app.use('/', serveStatic('client/dist'));
+    app.use('/', serveStatic('client/dist'));
 
-auth(app, {
-    session: {secret: config.get('auth.sessionSecret')}
-});
+    if (withAuth) {
+        auth(app, {
+            session: {secret: config.get('auth.sessionSecret')}
+        });
+    }
 
-app.use('/api/v1/config', routes.config);
-app.use('/api/v1/app', routes.apps);
-app.use('/api/v1/template', routes.templates);
-app.use('/api/v1/route', routes.appRoutes);
-app.use('/api/v1/shared_props', routes.sharedProps);
+    app.use('/api/v1/config', routes.config);
+    app.use('/api/v1/app', routes.apps);
+    app.use('/api/v1/template', routes.templates);
+    app.use('/api/v1/route', routes.appRoutes);
+    app.use('/api/v1/shared_props', routes.sharedProps);
 
-app.use(errorHandler);
+    app.use(errorHandler);
 
-app.disable('x-powered-by');
+    app.disable('x-powered-by');
 
-export default app;
+    return app;
+}
