6d62692f028bedf39d33fe4b5c04e4ec15ce1f53
Volodymyr Makukha <nei.css@gmail.com>
2020-04-15 19:27:42 +0300

    chore: use cidr lib

--- ilc/server/tailor/parse-override-config.js
+++ ilc/server/tailor/parse-override-config.js
@@ -1,14 +1,17 @@
 const _ = require('lodash');
+const CIDRMatcher = require('cidr-matcher');
 
-const fullRange = '(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])';
-const privateNetworks = [
-  new RegExp(`^https?:\\/\\/10\\.${fullRange}\\.${fullRange}\\.${fullRange}`), // 10.0.0.0
-  new RegExp(`^https?:\\/\\/192\\.168\\.${fullRange}\\.${fullRange}`), // 192.168.0.0
-  new RegExp(`^https?:\\/\\/172\\.(1[6-9]|2[0-9]|3[01])\\.${fullRange}\\.${fullRange}`), // 172.16.0.0 - 172.31.255.255
-  new RegExp('^https?:\\/\\/127\\.0\\.0\\.1'), // 127.0.0.1
-];
+var privateNetworks = new CIDRMatcher([
+    '10.0.0.0/8',
+    '192.168.0.0/16',
+    '172.16.0.0/12',
+    '127.0.0.0/8',
+]);
 
-const isPrivateNetwork = link => privateNetworks.some(re => re.test(link));
+const isPrivateNetwork = link => {
+    const matchedIp = link.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/);
+    return matchedIp && matchedIp[0] && privateNetworks.contains(matchedIp[0]);
+}
 const isLink = text => typeof text === 'string' && /https?:\/\/?/.test(text);
 
 const sanitizeSpoofedLinks = obj => {
@@ -21,7 +24,7 @@ const sanitizeSpoofedLinks = obj => {
     });
 };
 
-export default cookie => {
+module.exports = cookie => {
     try {
         let overrideConfig = typeof cookie === 'string' && cookie.split(';').find(n => n.trim().startsWith('ILC-overrideConfig'));
         if (overrideConfig) {
