33c110881d0cc56e5374fa5e5e02d093b9d26723
Volodymyr Makukha <nei.css@gmail.com>
2020-04-16 11:25:24 +0300

    chore: small fixes

--- ilc/server/tailor/parse-override-config.js
+++ ilc/server/tailor/parse-override-config.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 const CIDRMatcher = require('cidr-matcher');
 
-var privateNetworks = new CIDRMatcher([
+const privateNetworks = new CIDRMatcher([
     '10.0.0.0/8',
     '192.168.0.0/16',
     '172.16.0.0/12',
@@ -12,7 +12,7 @@ const isPrivateNetwork = link => {
     const matchedIp = link.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/);
     return matchedIp && matchedIp[0] && privateNetworks.contains(matchedIp[0]);
 }
-const isLink = text => typeof text === 'string' && /https?:\/\/?/.test(text);
+const isLink = text => typeof text === 'string' && /^https?:\/\/?/.test(text);
 
 const sanitizeSpoofedLinks = obj => {
     Object.entries(obj).forEach(([key, value]) => {
@@ -28,7 +28,7 @@ module.exports = cookie => {
     try {
         let overrideConfig = typeof cookie === 'string' && cookie.split(';').find(n => n.trim().startsWith('ILC-overrideConfig'));
         if (overrideConfig) {
-            overrideConfig = JSON.parse(decodeURIComponent(overrideConfig.replace(/^\s?ILC\-overrideConfig=/, '')));
+            overrideConfig = JSON.parse(decodeURIComponent(overrideConfig.replace(/^\s?ILC-overrideConfig=/, '')));
             overrideConfig.apps && sanitizeSpoofedLinks(overrideConfig.apps);
             return overrideConfig;
         }
