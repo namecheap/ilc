4b85e275d9011b09c395f34eca024c191e360e11
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-04-17 12:02:52 +0300

    fix: fixed seeds for sqlite DB

--- registry/server/seeds/00_cleanup.ts
+++ registry/server/seeds/00_cleanup.ts
@@ -2,7 +2,7 @@ import * as Knex from "knex";
 
 export async function seed(knex: Knex): Promise<any> {
     return knex.transaction(async function (trx) {
-        await knex.schema.raw('SET FOREIGN_KEY_CHECKS = 0;').transacting(trx);
+        isMySQL(knex) && await knex.schema.raw('SET FOREIGN_KEY_CHECKS = 0;').transacting(trx);
 
         try {
             await knex('route_slots').transacting(trx).truncate();
@@ -10,7 +10,11 @@ export async function seed(knex: Knex): Promise<any> {
             await knex('apps').transacting(trx).truncate();
             await knex('templates').transacting(trx).truncate();
         } finally {
-            await knex.schema.raw('SET FOREIGN_KEY_CHECKS = 1;').transacting(trx);
+            isMySQL(knex) && await knex.schema.raw('SET FOREIGN_KEY_CHECKS = 1;').transacting(trx);
         }
     });
+}
+
+function isMySQL(knex: Knex) {
+    return ["mysql", "mariasql", "mariadb"].indexOf(knex.client.dialect) > -1;
 }
