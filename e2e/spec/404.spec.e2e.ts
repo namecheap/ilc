8279da0e7f7a129208dbb44f14bcb54e39fc1743
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-08-07 14:12:41 +0300

    chore: e2e tests refactoring

--- e2e/spec/404.spec.e2e.ts
+++ e2e/spec/404.spec.e2e.ts
@@ -22,62 +22,62 @@ Scenario('Renders (SSR) global 404 page for non-existing News app route', (I) =>
 });
 
 Scenario('Renders (CSR) global 404 page for non-existing News app route', (I, newsPage: newsPage) => {
-    I.amOnPage(newsPage.newsUrl);
-    I.waitInUrl(newsPage.newsUrl, 10);
-    I.waitForElement(newsPage.goToNonExistingRoute, 10);
-    I.click(newsPage.goToNonExistingRoute);
+    I.amOnPage(newsPage.url.main);
+    I.waitInUrl(newsPage.url.main, 10);
+    I.waitForElement(newsPage.linkWithUrl(newsPage.url.nonExistingRoute), 10);
+    I.click(newsPage.linkWithUrl(newsPage.url.nonExistingRoute));
     I.waitForText('404 not found', 10, 'body > div#body');
 
     //After 404 page ILC continues normal operation
-    I.click(newsPage.goToNews);
+    I.click(newsPage.linkWithUrl(newsPage.url.main));
     I.waitForElement(newsPage.newsSources, 10);
     I.see('Pick a news source', newsPage.bannerHeadline);
 });
 
 Scenario('Renders (SSR) overridden 404 page for non-existing News app route', (I, newsPage: newsPage) => {
-    I.amOnPage('/news/nonExisting?overrideErrorPage=1');
+    I.amOnPage(newsPage.url.nonExistingRouteWithOverride);
     I.waitForText('404 not found component', 10, 'body > div#body');
 });
 
 Scenario('Renders (CSR) overridden 404 page for non-existing News app route', (I, newsPage: newsPage) => {
-    I.amOnPage(newsPage.newsUrl);
-    I.waitInUrl(newsPage.newsUrl, 10);
-    I.waitForElement(newsPage.goToNonExistingRouteOverride, 10);
-    I.click(newsPage.goToNonExistingRouteOverride);
+    I.amOnPage(newsPage.url.main);
+    I.waitInUrl(newsPage.url.main, 10);
+    I.waitForElement(newsPage.linkWithUrl(newsPage.url.nonExistingRouteWithOverride), 10);
+    I.click(newsPage.linkWithUrl(newsPage.url.nonExistingRouteWithOverride));
     I.waitForText('404 not found component', 10, 'body > div#body');
 });
 //endregion 404 page for non-existing News app route
 
 //region 404 page for non-existing News resource
-Scenario('Renders (SSR) global 404 page for non-existing News resource', (I) => {
-    I.amOnPage('/news/article/abc-news-au34');
+Scenario('Renders (SSR) global 404 page for non-existing News resource', (I, newsPage: newsPage) => {
+    I.amOnPage(newsPage.url.nonExistingResource);
     I.waitForText('404 not found', 10, 'body > div#body');
 });
 
 Scenario('Renders (CSR) global 404 page for non-existing News resource', (I, newsPage: newsPage) => {
-    I.amOnPage(newsPage.newsUrl);
-    I.waitInUrl(newsPage.newsUrl, 10);
-    I.waitForElement(newsPage.goToNonExistingResource, 10);
-    I.click(newsPage.goToNonExistingResource);
+    I.amOnPage(newsPage.url.main);
+    I.waitInUrl(newsPage.url.main, 10);
+    I.waitForElement(newsPage.linkWithUrl(newsPage.url.nonExistingResource), 10);
+    I.click(newsPage.linkWithUrl(newsPage.url.nonExistingResource));
     I.waitForText('404 not found', 10, 'body > div#body');
 
     //After 404 page ILC continues normal operation
     I.wait(5); //Hack to fix issue with the Vue Router
-    I.click(newsPage.goToNews);
+    I.click(newsPage.linkWithUrl(newsPage.url.main));
     I.waitForElement(newsPage.newsSources, 10);
     I.see('Pick a news source', newsPage.bannerHeadline);
 });
 
 Scenario('Renders (SSR) overridden 404 page for non-existing News resource', (I, newsPage: newsPage) => {
-    I.amOnPage('/news/article/abc-news-au34?overrideErrorPage=1');
+    I.amOnPage(newsPage.url.nonExistingResourceWithOverride);
     I.waitForText('404 not found component', 1000, 'body > div#body');
 });
 
 Scenario('Renders (CSR) overridden 404 page for non-existing News resource', (I, newsPage: newsPage) => {
-    I.amOnPage(newsPage.newsUrl);
-    I.waitInUrl(newsPage.newsUrl, 10);
-    I.waitForElement(newsPage.goToNonExistingResourceOverride, 10);
-    I.click(newsPage.goToNonExistingResourceOverride);
+    I.amOnPage(newsPage.url.main);
+    I.waitInUrl(newsPage.url.main, 10);
+    I.waitForElement(newsPage.linkWithUrl(newsPage.url.nonExistingResourceWithOverride), 10);
+    I.click(newsPage.linkWithUrl(newsPage.url.nonExistingResourceWithOverride));
     I.waitForText('404 not found component', 10, 'body > div#body');
 });
 //endregion 404 page for non-existing News resource
--- e2e/spec/pages/news.ts
+++ e2e/spec/pages/news.ts
@@ -1,10 +1,16 @@
 const { I } = inject();
 
-export const newsUrl = '/news/';
+export const url = {
+    main: '/news/',
+    nonExistingRoute: '/news/nonExisting',
+    nonExistingRouteWithOverride: '/news/nonExisting?overrideErrorPage=1',
+    nonExistingResource: '/news/article/abc-news-au34',
+    nonExistingResourceWithOverride: '/news/article/abc-news-au34?overrideErrorPage=1',
+};
 
-export const goToNews = `body > div#navbar a[href="${newsUrl}"]`;
+export const linkWithUrl = (url:string) => `a[href="${url}"]`;
+
 export const newsView = 'body > div#body > div.single-spa-container.news-app > div.view';
-export const goToNewsSources = `${newsView} > div.container > p.home > a[href="${newsUrl}"]`;
 export const newsSources = `${newsView} > div.sources > div.container > ol > li.source`;
 export const bannerHeadline = `${newsView} > div.banner > h1`;
 export const generateError = `${newsView} > div.banner > a`;
@@ -13,7 +19,3 @@ export const lastNewsSource = `${newsSources}:last-child`;
 export const lastNewsSourceLink = `${lastNewsSource} > p.action > a`;
 export const newsSourceArticles = `${newsView} > div.container > div.articles > ol > li.article`;
 export const firstNewsSourceArticle = `${newsSourceArticles}:first-child > div.meta > div.redirect > a`;
-export const goToNonExistingRoute = `${newsView} a[href="/news/nonExisting"]`;
-export const goToNonExistingRouteOverride = `${newsView} a[href="/news/nonExisting?overrideErrorPage=1"]`;
-export const goToNonExistingResource = `${newsView} a[href="/news/article/abc-news-au34"]`;
-export const goToNonExistingResourceOverride = `${newsView} a[href="/news/article/abc-news-au34?overrideErrorPage=1"]`;
