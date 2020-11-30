75de4936ab812377faa26009181aa28a9dd0121a
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-11-30 14:20:49 +0200

    feat: i18n now is enabled by default

---
+++ registry/server/migrations/20201130141801_i18n_enabled_by_default.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+import {SettingKeys} from "../settings/interfaces";
+
+
+export async function up(knex: Knex): Promise<any> {
+    await knex('settings').update({
+        value: JSON.stringify(true),
+        default: JSON.stringify(true),
+    }).where('key', SettingKeys.I18nEnabled);
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+    await knex('settings').update({
+        value: JSON.stringify(false),
+        default: JSON.stringify(false),
+    }).where('key', SettingKeys.I18nEnabled);
+}
+
