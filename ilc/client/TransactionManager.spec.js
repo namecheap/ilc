fd076eba1f74058fcc972892d563cb631cd8f75c
Volodymyr Makukha <nei.css@gmail.com>
2020-12-10 12:38:20 +0200

    fix: transition between pages in different sequence

--- ilc/client/TransactionManager.spec.js
+++ ilc/client/TransactionManager.spec.js
@@ -215,57 +215,169 @@ describe('TransactionManager', () => {
         chai.expect(spinner.getRef()).to.be.null;
     });
 
-    it('should render a fake slot and listen to slot content changes when a slot is going to be rerendered', async () => {
-        const newBodyApplication = {
-            id: 'new-body-application',
-            class: 'new-body-spa',
-        };
+    // can be case when rendering is triggered before removing and vise versa, but the result should be always the same
+    describe('should render a fake slot and listen to slot content changes', () => {
+        it('when a slot is going to be rerendered', async () => {
+            const newBodyApplication = {
+                id: 'new-body-application',
+                class: 'new-body-spa',
+            };
 
-        newBodyApplication.ref = html`
-            <div id="${newBodyApplication.id}" class="${newBodyApplication.class}">
-                Hello! I am new Body SPA
-            </div>
-        `;
+            newBodyApplication.ref = html`
+                <div id="${newBodyApplication.id}" class="${newBodyApplication.class}">
+                    Hello! I am new Body SPA
+                </div>
+            `;
 
-        applications.navbar.appendApplication();
-        applications.body.appendApplication();
+            applications.navbar.appendApplication();
+            applications.body.appendApplication();
 
-        handlePageTransaction(slots.body.id, slotWillBe.rerendered);
+            handlePageTransaction(slots.body.id, slotWillBe.rerendered);
 
-        await clock.runAllAsync();
+            await clock.runAllAsync();
 
-        applications.body.removeApplication();
+            applications.body.removeApplication();
 
-        await clock.runAllAsync();
+            await clock.runAllAsync();
 
-        const bodyApplications = document.getElementsByClassName(applications.body.class);
-        chai.expect(bodyApplications.length).to.be.equal(1);
+            const bodyApplications = document.getElementsByClassName(applications.body.class);
+            chai.expect(bodyApplications.length).to.be.equal(1);
 
-        const [fakeBodyApplicationRef] = bodyApplications;
-        const fakeBodySlot = fakeBodyApplicationRef.parentNode;
+            const [fakeBodyApplicationRef] = bodyApplications;
+            const fakeBodySlot = fakeBodyApplicationRef.parentNode;
 
-        chai.expect(fakeBodyApplicationRef.id).to.be.equal(applications.body.id);
+            chai.expect(fakeBodyApplicationRef.id).to.be.equal(applications.body.id);
 
-        chai.expect(window.getComputedStyle(fakeBodyApplicationRef, null).display).to.be.equal('block');
-        chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
-        chai.expect(slots.body.getComputedStyle().display).to.be.equal('none');
-        chai.expect(window.getComputedStyle(fakeBodySlot, null).display).to.be.equal('block');
+            chai.expect(window.getComputedStyle(fakeBodyApplicationRef, null).display).to.be.equal('block');
+            chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getComputedStyle().display).to.be.equal('none');
+            chai.expect(window.getComputedStyle(fakeBodySlot, null).display).to.be.equal('block');
 
-        chai.expect(fakeBodySlot.nodeName).to.be.equal(slots.body.ref.nodeName);
-        chai.expect(fakeBodySlot.id).to.be.equal('');
-        chai.expect(fakeBodySlot.className).to.be.equal('');
+            chai.expect(fakeBodySlot.nodeName).to.be.equal(slots.body.ref.nodeName);
+            chai.expect(fakeBodySlot.id).to.be.equal('');
+            chai.expect(fakeBodySlot.className).to.be.equal('');
 
-        chai.expect(spinner.getRef()).to.be.not.null;
-        chai.expect(slots.body.getAttributeName()).to.be.equal(locationHash);
+            chai.expect(spinner.getRef()).to.be.not.null;
+            chai.expect(slots.body.getAttributeName()).to.be.equal(locationHash);
 
-        slots.body.ref.appendChild(newBodyApplication.ref);
+            slots.body.ref.appendChild(newBodyApplication.ref);
 
-        await clock.runAllAsync();
+            await clock.runAllAsync();
 
-        chai.expect(spinner.getRef()).to.be.null;
-        chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
-        chai.expect(slots.body.getComputedStyle().display).to.be.equal('block');
-        chai.expect(slots.body.getAttributeName()).to.be.null;
+            chai.expect(spinner.getRef()).to.be.null;
+            chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getAttributeName()).to.be.null;
+        });
+
+        // default behaviour, now it equals to "rerendered", but it's good to have test
+        it('when a slot is going to be removed and then rendered', async () => {
+            const newBodyApplication = {
+                id: 'new-body-application',
+                class: 'new-body-spa',
+            };
+
+            newBodyApplication.ref = html`
+                <div id="${newBodyApplication.id}" class="${newBodyApplication.class}">
+                    Hello! I am new Body SPA
+                </div>
+            `;
+
+            applications.navbar.appendApplication();
+            applications.body.appendApplication();
+
+            handlePageTransaction(slots.body.id, slotWillBe.removed);
+            handlePageTransaction(slots.body.id, slotWillBe.rendered);
+
+            await clock.runAllAsync();
+
+            applications.body.removeApplication();
+
+            await clock.runAllAsync();
+
+            const bodyApplications = document.getElementsByClassName(applications.body.class);
+            chai.expect(bodyApplications.length).to.be.equal(1);
+
+            const [fakeBodyApplicationRef] = bodyApplications;
+            const fakeBodySlot = fakeBodyApplicationRef.parentNode;
+
+            chai.expect(fakeBodyApplicationRef.id).to.be.equal(applications.body.id);
+
+            chai.expect(window.getComputedStyle(fakeBodyApplicationRef, null).display).to.be.equal('block');
+            chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getComputedStyle().display).to.be.equal('none');
+            chai.expect(window.getComputedStyle(fakeBodySlot, null).display).to.be.equal('block');
+
+            chai.expect(fakeBodySlot.nodeName).to.be.equal(slots.body.ref.nodeName);
+            chai.expect(fakeBodySlot.id).to.be.equal('');
+            chai.expect(fakeBodySlot.className).to.be.equal('');
+
+            chai.expect(spinner.getRef()).to.be.not.null;
+            chai.expect(slots.body.getAttributeName()).to.be.equal(locationHash);
+
+            slots.body.ref.appendChild(newBodyApplication.ref);
+
+            await clock.runAllAsync();
+
+            chai.expect(spinner.getRef()).to.be.null;
+            chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getAttributeName()).to.be.null;
+        });
+
+        it('when a slot is going to be rendered and then removed', async () => {
+            const newBodyApplication = {
+                id: 'new-body-application',
+                class: 'new-body-spa',
+            };
+
+            newBodyApplication.ref = html`
+                <div id="${newBodyApplication.id}" class="${newBodyApplication.class}">
+                    Hello! I am new Body SPA
+                </div>
+            `;
+
+            applications.navbar.appendApplication();
+            applications.body.appendApplication();
+
+            handlePageTransaction(slots.body.id, slotWillBe.rendered);
+            handlePageTransaction(slots.body.id, slotWillBe.removed);
+
+            await clock.runAllAsync();
+
+            applications.body.removeApplication();
+
+            await clock.runAllAsync();
+
+            const bodyApplications = document.getElementsByClassName(applications.body.class);
+            chai.expect(bodyApplications.length).to.be.equal(1);
+
+            const [fakeBodyApplicationRef] = bodyApplications;
+            const fakeBodySlot = fakeBodyApplicationRef.parentNode;
+
+            chai.expect(fakeBodyApplicationRef.id).to.be.equal(applications.body.id);
+
+            chai.expect(window.getComputedStyle(fakeBodyApplicationRef, null).display).to.be.equal('block');
+            chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getComputedStyle().display).to.be.equal('none');
+            chai.expect(window.getComputedStyle(fakeBodySlot, null).display).to.be.equal('block');
+
+            chai.expect(fakeBodySlot.nodeName).to.be.equal(slots.body.ref.nodeName);
+            chai.expect(fakeBodySlot.id).to.be.equal('');
+            chai.expect(fakeBodySlot.className).to.be.equal('');
+
+            chai.expect(spinner.getRef()).to.be.not.null;
+            chai.expect(slots.body.getAttributeName()).to.be.equal(locationHash);
+
+            slots.body.ref.appendChild(newBodyApplication.ref);
+
+            await clock.runAllAsync();
+
+            chai.expect(spinner.getRef()).to.be.null;
+            chai.expect(slots.navbar.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getComputedStyle().display).to.be.equal('block');
+            chai.expect(slots.body.getAttributeName()).to.be.null;
+        });
     });
 
     it('should destroy spinner when all fragments contain text nodes', async () => {
