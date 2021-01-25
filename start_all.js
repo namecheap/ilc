3bcfcfcb0e0a86cac2afd4085602e06d8d0af5e4
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-01-25 16:39:47 +0200

    chore: support for older Docker CLI

--- start_all.js
+++ start_all.js
@@ -20,7 +20,7 @@ if (!noWatch) {
     commands.push({ command: 'cd ./registry/client && npm run build:watch', name: 'registry:ui' });
 }
 if (runWithApps) {
-    commands.push({ command: 'docker run --pull always --rm -p 8235-8240:8235-8240 namecheap/ilc-demo-apps', name: 'demo-apps' });
+    commands.push({ command: 'docker run --rm -p 8235-8240:8235-8240 namecheap/ilc-demo-apps', name: 'demo-apps' });
 }
 
 concurrently(commands, {
