4fa13ab428dd7b30b1b7c937d31c567f7cd40880
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-01-28 12:59:41 +0200

    chore: E2E tests updated to new Demo apps

--- e2e/spec/i18n.spec.e2e.ts
+++ e2e/spec/i18n.spec.e2e.ts
@@ -2,23 +2,23 @@ Feature('I18n');
 
 Scenario('Renders (SSR) default language', (I) => {
     I.amOnPage('/news/');
-    I.see('People', '#navbar .primary-navigation-link');
+    I.see('No SSR', '#navbar .primary-navigation-link');
 });
 
 Scenario('Renders (SSR) UA language', (I) => {
     I.amOnPage('/ua/news/');
-    I.see('Люди', '#navbar .primary-navigation-link');
+    I.see('Без SSR', '#navbar .primary-navigation-link');
 });
 
 Scenario('Switches language there and backwards', (I) => {
     I.amOnPage('/news/');
 
     I.click('UA', '#navbar');
-    I.see('Люди', '#navbar .primary-navigation-link');
+    I.see('Без SSR', '#navbar .primary-navigation-link');
     I.seeInCurrentUrl('/ua/news/');
 
     I.click('EN', '#navbar');
-    I.see('People', '#navbar .primary-navigation-link');
+    I.see('No SSR', '#navbar .primary-navigation-link');
     I.seeInCurrentUrl('/news/');
 });
 
