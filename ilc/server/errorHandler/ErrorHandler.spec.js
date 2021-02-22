c8354040c8f4c11d63fadf8248b2450bc62ce629
Oleh Momot <oleh_momot@pm.me>
2021-02-22 19:28:31 +0200

    fix: remove tests from ErrorHandler

--- ilc/server/errorHandler/ErrorHandler.spec.js
+++ ilc/server/errorHandler/ErrorHandler.spec.js
@@ -1,29 +1,18 @@
 const chai = require('chai');
 const supertest = require('supertest');
 const nock = require('nock');
-const stdoutInterceptor = require("test-console").stdout;
 const config = require('config');
+
 const localStorage = require('../../common/localStorage');
 const helpers = require('../../tests/helpers');
 
 const createApp = require('../app');
 
-const parseOutput = capturedOutput => capturedOutput.map(v => {
-    v = Buffer.from(v).toString('utf-8');
-    try {
-        return JSON.parse(v);
-    } catch (e) {
-        return v;
-    }
-
-});
-
-describe('error handler', () => {
+describe('ErrorHandler', () => {
     const errorIdRegExp = /(?<errorId>[\d\w]{8}-[\d\w]{4}-[\d\w]{4}-[\d\w]{4}-[\d\w]{12})/;
 
     let app;
     let server;
-    let stdoutInspect;
 
     before(async () => {
         app = createApp(helpers.getRegistryMock(), helpers.getPluginManagerMock());
@@ -31,13 +20,8 @@ describe('error handler', () => {
         server = supertest(app.server);
     });
 
-    beforeEach(() => {
-        stdoutInspect = stdoutInterceptor.inspect();
-    });
-
     afterEach(() => {
         localStorage.clear();
-        stdoutInspect.restore();
     });
 
     after(() => {
@@ -72,23 +56,6 @@ describe('error handler', () => {
             '</body>' +
             '</html>'
         );
-
-        const output = parseOutput(stdoutInspect.output);
-
-        chai.expect(output[1]).to.deep.include({
-            level: 50,
-            type: 'Error',
-            message: '500 page test error',
-            additionalInfo: {
-                errorId,
-                reqId: output[1].additionalInfo.reqId || 'some value should be here',
-            },
-        });
-
-        chai.expect(output[1].stack[0]).to.eql('Error: 500 page test error');
-        chai.expect(output[1]).to.have.property('time');
-        chai.expect(output[1]).to.have.property('pid');
-        chai.expect(output[1]).to.have.property('hostname');
     });
 
     it('should send an error message when showing 500 error page throws an error', async () => {
@@ -97,19 +64,7 @@ describe('error handler', () => {
         nock(config.get('registry').address).get(`/api/v1/template/500/rendered`).replyWithError(replyingError.message);
 
         const response = await server.get('/_ilc/500').expect(500);
-        const output = parseOutput(stdoutInspect.output);
 
         chai.expect(response.text).to.be.eql('Oops! Something went wrong. Pls try to refresh page or contact support.');
-        chai.expect(output[1]).to.deep.include({
-            level: 50,
-            type: 'Error',
-            message: '500 page test error',
-        });
-        chai.expect(output[1]).to.have.nested.property('additionalInfo.errorId');
-        chai.expect(output[2].stack.join("\n")).to.have.string("ErrorHandlingError: \n");
-        chai.expect(output[2].stack.join("\n")).to.have.string("Caused by: Error: Something awful happened.\n");
-        chai.expect(output[2]).to.have.property('time');
-        chai.expect(output[2]).to.have.property('pid');
-        chai.expect(output[2]).to.have.property('hostname');
     });
 });
