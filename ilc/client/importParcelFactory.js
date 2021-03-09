13d2b23586337fb581151a2a6b267ed2ea5cb771
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-03-09 18:29:45 +0200

    chore: added injection of all ILC props

--- ilc/client/importParcelFactory.js
+++ ilc/client/importParcelFactory.js
@@ -1,4 +1,7 @@
-import {flattenFnArray} from "./utils";
+import {flattenFnArray} from './utils';
+import {Intl as IlcIntl} from 'ilc-sdk/app';
+// TODO: make correct export from ilc-sdk
+import defaultIntlAdapter from 'ilc-sdk/dist/app/defaultIntlAdapter';
 
 export default (registryConf, bundleLoader) => async (appName, parcelName) => {
     const app = registryConf.apps[appName];
@@ -14,20 +17,40 @@ export default (registryConf, bundleLoader) => async (appName, parcelName) => {
 
     const parcelCallbacks = appBundle.parcels[parcelName];
 
+    let intlInstances = {};
 
-    return propsInjector(parcelCallbacks, {
-        test: 1
+    return propsInjector(parcelCallbacks, (props, lifecycleType) => {
+        let intlForUnmount;
+        if (lifecycleType === 'unmount') {
+            intlForUnmount = intlInstances[props.name];
+            if (intlForUnmount) {
+                intlForUnmount.unmount();
+            }
+            delete intlInstances[props.name];
+        } else if (!intlInstances[props.name]) {
+            const adapter = window.ILC.getAppSdkAdapter(props.name) || defaultIntlAdapter;
+            intlInstances[props.name] = new IlcIntl(props.name, adapter.intl);
+        }
+
+
+        return {
+            parcelSdk: {
+                parcelId: props.name,
+                registryProps: app.props,
+                intl: intlInstances[props.name] || intlForUnmount,
+            },
+        };
     });
 };
 
-function propsInjector(callbacks, extraProps) {
+function propsInjector(callbacks, extraPropsCb) {
     for (let lifecycle in callbacks) {
         if (!callbacks.hasOwnProperty(lifecycle)) {
             continue;
         }
 
         const callback = flattenFnArray(callbacks, lifecycle);
-        callbacks[lifecycle] = (props) => callback({...props, ...extraProps});
+        callbacks[lifecycle] = (props) => callback({...props, ...extraPropsCb(props, lifecycle)});
     }
 
     return callbacks;
