91bee7541afaaa986ac75370f0c0eb240856763f
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-05-05 18:32:15 +0300

    chore: fixed unique index for orderPos and route columns in routes table

--- registry/server/migrations/20210430155514_routes_removeColumnSpecialRole.ts
+++ registry/server/migrations/20210430155514_routes_removeColumnSpecialRole.ts
@@ -1,9 +1,10 @@
 import * as Knex from "knex";
+import isMySQL from "../util/isMySQL";
 
 export async function up(knex: Knex): Promise<any> {
     return knex.schema.table('routes', function (table) {
         table.dropColumn('specialRole');
-        
+
         if (!isMySQL(knex)) {
             // SQLite drops "unique" during dropping column, so we need to reset it
             table.unique(['orderPos'], 'routes_orderpos_unique');
@@ -17,8 +18,4 @@ export async function down(knex: Knex): Promise<any> {
     return knex.schema.table('routes', function (table) {
         table.enum('specialRole', ['404']).nullable();
     });
-}
-
-function isMySQL(knex: Knex) {
-    return ["mysql", "mariasql", "mariadb"].indexOf(knex.client.dialect) > -1;
 }
---
+++ registry/server/migrations/20210505182425_old_tbls_cleanup.ts
@@ -0,0 +1,20 @@
+import * as Knex from "knex";
+import isMySQL from "../util/isMySQL";
+
+
+export async function up(knex: Knex): Promise<void> {
+    if (!isMySQL(knex)) {
+        await knex.schema.dropTable('old_route_slots');
+        await knex.schema.dropTable('old_route_slots_2');
+        await knex.schema.dropTable('old_routes');
+        await knex.schema.dropTable('old_routes_2');
+    }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    if (!isMySQL(knex)) {
+        throw new Error('Not implemented');
+    }
+}
+
