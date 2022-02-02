f170443cf54ebf9c6a0bdb0d6a249afae99e18b6
Vladlen Fedosov <vladlen.f@namecheap.com>
2022-02-02 19:44:11 +0200

    chore: upgrade to latest sqlite3

--- registry/server/migrations/20210430155514_routes_removeColumnSpecialRole.ts
+++ registry/server/migrations/20210430155514_routes_removeColumnSpecialRole.ts
@@ -4,12 +4,6 @@ import isMySQL from "../util/isMySQL";
 export async function up(knex: Knex): Promise<any> {
     return knex.schema.table('routes', function (table) {
         table.dropColumn('specialRole');
-
-        if (!isMySQL(knex)) {
-            // SQLite drops "unique" during dropping column, so we need to reset it
-            table.unique(['orderPos'], 'routes_orderpos_unique');
-            table.unique(['route', 'domainId'], 'routes_route_and_domainId_unique');
-        }
     });
 }
 
