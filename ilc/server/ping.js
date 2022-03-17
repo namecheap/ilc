a947ff111924a2e40903c5b01875abeabaed8704
Artyom Zakharchenko <artyom.zakharchenko@namecheap.com>
2022-03-17 15:59:41 +0200

    chore; remove tech logs

--- ilc/server/ping.js
+++ ilc/server/ping.js
@@ -2,7 +2,6 @@ const registryService = require('./registry/factory');
 
 module.exports = function (fastify, opts, done) {
     fastify.get('/ping', async (req, res) => {
-        req.log.info('test');
         await registryService.preheat();
         res.status(200).send('pong');
     });
