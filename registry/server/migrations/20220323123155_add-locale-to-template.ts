6f369278a6ccc027eda05f95a06efb9bb25e4165
Evgeniy Moroz <evgeniy.moroz@namecheap.com>
2022-03-28 11:20:41 +0300

    chore: fix size of content for localized templates, since default text allows only 64kb of content.

--- registry/server/migrations/20220323123155_add-locale-to-template.ts
+++ registry/server/migrations/20220323123155_add-locale-to-template.ts
@@ -5,7 +5,7 @@ import { tables } from '../db/structure';
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable(tables.templatesLocalized, table => {
         table.string('templateName', 50).notNullable().references('templates.name').onDelete('CASCADE');
-        table.text('content').notNullable();
+        table.text('content', 'mediumtext').notNullable();
         table.string('locale', 7).notNullable();
         table.unique(['templateName', 'locale']);
     });
