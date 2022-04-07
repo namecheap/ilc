dc9f50944688c191c555fc7247dd9ed84e752242
Serhii Chumak <serhii.chumak@namecheap.com>
2022-04-07 12:26:55 +0300

    refactor: config filters

--- registry/server/config/filters/SpecialRoutesFilter.ts
+++ registry/server/config/filters/SpecialRoutesFilter.ts
@@ -4,15 +4,15 @@ import { BaseNodeFilter, Node } from "./BaseNodeFilter";
 type Result = Record<string, Partial<Node>>;
 
 export class SpecialRoutesFilter extends BaseNodeFilter {
-    public static readonly accessPath = 'specialRoutes' as const;
+    public readonly accessPath = 'specialRoutes' as const;
 
-    public filter(predicate: string): Result {
+    public filter(node: Array<Node>): Result {
         const accum: [Result, Result] = [{}, {}];
-        const [ withDomain, withoutDomain ] = this.node.reduce(
+        const [ withDomain, withoutDomain ] = node.reduce(
             ([ left, right ], { domain, specialRole, ...rest }) => {
                 if(isString(specialRole)) {
                     !domain ? right[specialRole] = rest : (
-                        domain === predicate ? left[specialRole] = rest : null
+                        domain === this.predicate ? left[specialRole] = rest : null
                     )
                 }
 
