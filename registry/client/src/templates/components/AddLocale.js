b9800ce7faf0359652926872c89785dce5f0b791
Evgeniy Moroz <evgeniy.moroz@namecheap.com>
2022-04-27 18:57:54 +0300

    fix: allow to edit only supported locales for template on the client side.

--- registry/client/src/templates/components/AddLocale.js
+++ registry/client/src/templates/components/AddLocale.js
@@ -1,5 +1,16 @@
-import React, { useState } from 'react';
-import { Box, Button, Grid, Modal, TextField as MuiTextField, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import {
+    Box,
+    Button,
+    FormControl,
+    Grid, InputLabel,
+    MenuItem,
+    Modal,
+    Select,
+    TextField as MuiTextField,
+    Typography
+} from '@material-ui/core';
+import fetchJson from '../../httpClient';
 
 const modalBoxStyle = {
     position: 'absolute',
@@ -15,6 +26,31 @@ const modalBoxStyle = {
 
 export function AddLocale(props) {
     const [addingLocale, setAddingLocale] = useState('');
+    const [supportedLocales, setSupportedLocales] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        Promise.all([
+            fetchJson('/api/v1/settings/i18n.supported.locale'),
+            fetchJson('/api/v1/settings/i18n.default.locale')
+        ])
+            .then(([supportedResponse, defaultResponse]) => {
+                let supportedLocalesSetting = supportedResponse.json.value;
+                if (!Array.isArray(supportedLocalesSetting)) {
+                    throw new Error('Unexpected response from API');
+                }
+
+                const defaultLocale = defaultResponse.json.value;
+                const localesToAdd = supportedLocalesSetting.filter(v => !props.currentLocales.includes(v) && defaultLocale !== v);
+                setSupportedLocales(localesToAdd);
+                if (localesToAdd.length === 0) {
+                    setError('No languages left to add. If you need to add a new language change `ilc.supported.locale` setting first.')
+                }
+            })
+            .catch(e => {
+                setError(`Unable to fetch supported language due to ${e.message}`)
+            });
+    })
 
     const addLang = (e) => {
         e.preventDefault();
@@ -29,8 +65,13 @@ export function AddLocale(props) {
             <form onSubmit={addLang}>
                 <Typography variant={'h5'}>Add new Language</Typography>
                 <Box pt={2}>
-                    <MuiTextField id="lang" label="Language, i.e. es-MX" value={props.value} fullWidth={true}
-                                  onChange={e => setAddingLocale(e.target.value)} autoFocus={true}/>
+                    {error ? <Typography>{error}</Typography> : (<FormControl fullWidth={true}>
+                        <InputLabel id="lang-input-label">Language, i.e. es-MX</InputLabel>
+                        <Select id="lang-select" labelId={"lang-input-label"} value={props.value}
+                                onChange={e => setAddingLocale(e.target.value)} fullWidth={true}>
+                            {supportedLocales.map(locale => <MenuItem key={locale} value={locale}>{locale}</MenuItem>)}
+                        </Select>
+                    </FormControl>)}
                 </Box>
                 <Box pt={2}>
                     <Grid container={true} direction={'row'} justifyContent={'flex-end'} spacing={2}>
