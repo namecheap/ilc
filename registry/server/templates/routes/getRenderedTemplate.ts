3048fe1f4e2caa616dbe703fb09d7b6ea5b23197
Egor Manzhula <egor.manzhula@namecheap.com>
2022-05-18 17:26:58 +0300

    fix(getRenderedTemplate): Applied changes after codereview

--- registry/server/templates/routes/getRenderedTemplate.ts
+++ registry/server/templates/routes/getRenderedTemplate.ts
@@ -3,7 +3,7 @@ import {
     Response,
 } from 'express';
 import Joi from 'joi';
-import _ from 'lodash/fp';
+import _, { LodashStubString } from 'lodash/fp';
 
 import noticeError from '../../errorHandler/noticeError';
 import db from '../../db';
@@ -28,8 +28,26 @@ const validateRequestBeforeGetTemplateRendered = validateRequestFactory([{
     selector: 'params',
 }]);
 
+async function getTemplateByDomain(domain: string, templateName: string): Promise<Template | undefined> {
+    const [domainItem] = await db.select('id').from<RouterDomains>('router_domains').where('domainName', String(domain));
+
+    const [template] = await db.select('templates.*').from<Template>('templates')
+                .join('routes', 'templates.name', 'routes.templateName')
+                .where({
+                    domainId: domainItem.id,
+                    name: templateName,
+                });
+
+    return template;
+}
+
+async function getTemplateByName(templateName: string): Promise<Template | undefined> {
+    const [template] = await db.select().from<Template>('templates').where('name', templateName);
+    return template;
+}
+
 async function getRenderedTemplate(req: Request<GetTemplateRenderedRequestParams>, res: Response): Promise<void> {
-    let defaultTemplate;
+    let template;
 
     const {
         name: templateName,
@@ -38,28 +56,20 @@ async function getRenderedTemplate(req: Request<GetTemplateRenderedRequestParams
     const { locale, domain } = req.query;
 
     if (domain) {
-        const [domainItem] = await db.select('id').from<RouterDomains>('router_domains').where('domainName', String(domain));
-
-        if (domainItem) {
-            [defaultTemplate] = await db.select('templates.*').from<Template>('templates')
-                .join('routes', 'templates.name', 'routes.templateName')
-                .where({
-                    domainId: domainItem.id,
-                    name: templateName,
-                });
-        }
+        template = await getTemplateByDomain(String(domain), templateName);
     }
 
-    if (!defaultTemplate) {
-        [defaultTemplate] = await db.select().from<Template>('templates').where('name', templateName);
+    if (!template) {
+        template = await getTemplateByName(templateName);
+        template && console.info(`Template ${templateName} is not attached to the domain, found by template name.`);
     }
 
-    if (!defaultTemplate) {
+    if (!template) {
         res.status(404).send('Not found');
         return;
     }
 
-    let content = defaultTemplate.content;
+    let content = template.content;
     if (locale) {
         const [localizedTemplate] = await db.select()
             .from<LocalizedTemplate>(tables.templatesLocalized)
@@ -73,7 +83,7 @@ async function getRenderedTemplate(req: Request<GetTemplateRenderedRequestParams
 
     try {
         const renderedTemplate = await renderTemplate(content);
-        res.status(200).send(_.assign(defaultTemplate, renderedTemplate));
+        res.status(200).send(_.assign(template, renderedTemplate));
     } catch (e) {
         if (e instanceof errors.FetchIncludeError) {
             res.status(503).send(e.message);
