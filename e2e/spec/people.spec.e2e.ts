5124c5ebd769217d6f961cc3dd0feb64f5417c2f
Artyom Zakharchenko <artyom.zakharchenko@namecheap.com>
2022-12-06 13:24:39 +0000

    test: repair ignored tests

--- e2e/spec/people.spec.e2e.ts
+++ e2e/spec/people.spec.e2e.ts
@@ -1,37 +1,37 @@
-// Feature('people ilc demo application');
+Feature('people ilc demo application');
 
-// Scenario('should open a people page without any selected planet initially', async ({I, peoplePage}) => {
-//     I.amOnPage('/');
-//     I.waitForElement(peoplePage.goToPeople, 30);
-//     I.click(peoplePage.goToPeople);
-//     I.waitInUrl(peoplePage.peopleUrl, 30);
-//     I.seeAttributesOnElements(peoplePage.goToPeople, {
-//         'aria-current': 'page',
-//     });
-//     I.see('No one selected', peoplePage.selectedPerson);
-//     I.waitForClickable(peoplePage.fetchMorePeople, 30);
-//     I.seeNumberOfVisibleElements(peoplePage.personsList, 10);
-// });
+Scenario('should open a people page without any selected planet initially', async ({I, peoplePage}) => {
+    I.amOnPage('/');
+    I.waitForElement(peoplePage.goToPeople, 30);
+    I.click(peoplePage.goToPeople);
+    I.waitInUrl(peoplePage.peopleUrl, 30);
+    I.seeAttributesOnElements(peoplePage.goToPeople, {
+        'aria-current': 'page',
+    });
+    I.see('No one selected', peoplePage.selectedPerson);
+    I.waitForClickable(peoplePage.fetchMorePeople, 30);
+    I.seeNumberOfVisibleElements(peoplePage.personsList, 10);
+});
 
-// Scenario('should show more persons', async ({I, peoplePage}) => {
-//     I.amOnPage(peoplePage.peopleUrl);
-//     I.waitForElement(peoplePage.fetchMorePeople, 30);
-//     I.waitForClickable(peoplePage.fetchMorePeople, 30);
-//     I.click(peoplePage.fetchMorePeople);
-//     I.waitForClickable(peoplePage.fetchMorePeople, 30);
-//     I.seeNumberOfVisibleElements(peoplePage.personsList, 20);
-//     I.stopMocking();
-// });
+Scenario('should show more persons', async ({I, peoplePage}) => {
+    I.amOnPage(peoplePage.peopleUrl);
+    I.waitForElement(peoplePage.fetchMorePeople, 30);
+    I.waitForClickable(peoplePage.fetchMorePeople, 30);
+    I.click(peoplePage.fetchMorePeople);
+    I.waitForClickable(peoplePage.fetchMorePeople, 30);
+    I.seeNumberOfVisibleElements(peoplePage.personsList, 20);
+    I.stopMocking();
+});
 
-// Scenario('should show person`s details', async ({I, peoplePage}) => {
-//     I.amOnPage(peoplePage.peopleUrl);
-//     I.waitNumberOfVisibleElements(peoplePage.personsList, 10, 30);
-//
-//     const lastPersonName = await I.grabTextFrom(peoplePage.lastPerson) as string;
-//     const lastPersonHref = await I.grabAttributeFrom(peoplePage.lastPerson, 'href');
-//
-//     I.click(peoplePage.lastPerson);
-//     I.waitInUrl(lastPersonHref, 30);
-//     I.dontSee('No one selected', peoplePage.selectedPerson);
-//     I.see(lastPersonName, peoplePage.selectedPerson);
-// });
+Scenario('should show person`s details', async ({I, peoplePage}) => {
+    I.amOnPage(peoplePage.peopleUrl);
+    I.waitNumberOfVisibleElements(peoplePage.personsList, 10, 30);
+
+    const lastPersonName = await I.grabTextFrom(peoplePage.lastPerson) as string;
+    const lastPersonHref = await I.grabAttributeFrom(peoplePage.lastPerson, 'href');
+
+    I.click(peoplePage.lastPerson);
+    I.waitInUrl(lastPersonHref, 30);
+    I.dontSee('No one selected', peoplePage.selectedPerson);
+    I.see(lastPersonName, peoplePage.selectedPerson);
+});
