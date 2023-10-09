c5ec67adfa9fbdc5ea83e5294f43f8b7a6bff85d
Stanislav Mishchyshyn <stanislav.mishchyshyn@namecheap.com>
2023-10-09 16:53:03 +0300

    chore(registry/client): support node v20
    clean webpack config

--- registry/client/webpack.config.js
+++ registry/client/webpack.config.js
@@ -1,15 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const IgnoreNotFoundExportPlugin = require('ignore-not-found-export-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
 
 module.exports = {
     devtool: 'source-map',
     output: {
-        devtoolNamespace: 'ilcRegistry'
+        devtoolNamespace: 'ilcRegistry',
+        assetModuleFilename: 'img/[hash][ext][query]'
     },
     module: {
         rules: [
@@ -27,53 +24,16 @@ module.exports = {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
-            {test: /\.svg$/, loader: 'file-loader'}
+            { test: /\.svg$/, loader: 'file-loader' },
         ],
     },
     plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, 'src/img/'), to: path.resolve(__dirname, 'dist/img/') },
-            ],
-        }),
         new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
-        // required because of https://github.com/babel/babel/issues/7640
-        new IgnoreNotFoundExportPlugin([
-            'CallbackSideEffect',
-            'ChoicesProps',
-            'InputProps',
-            'NotificationSideEffect',
-            'OptionText',
-            'OptionTextElement',
-            'RedirectionSideEffect',
-            'RefreshSideEffect',
-            'AdminUIProps',
-            'AdminContextProps',
-            'AdminRouterProps',
-            'ReferenceArrayProps',
-            'ReferenceManyProps',
-            'LinkToType',
-            'FormContext',
-            'UseReferenceProps',
-            'SortProps',
-            'PaginationProps',
-        ]),
-    ].concat(
-        process.env.NODE_ENV === 'development'
-            ? [new BundleAnalyzerPlugin()]
-            : []
-    ),
+    ].concat(process.env.NODE_ENV === 'development' ? [new BundleAnalyzerPlugin({ openAnalyzer: false })] : []),
     resolve: {
         extensions: ['.ts', '.js', '.tsx', '.json'],
-    },
-    devServer: {
-        stats: {
-            children: false,
-            chunks: false,
-            modules: false,
-        },
     },
 };
