507adb4f751b3a8a8f8d516cad5172c27e968bdb
Artyom Zakharchenko <artyom.zakharchenko@namecheap.com>
2023-11-20 16:14:38 +0000

    feat: add typescript compilation and build sources execution

--- ilc/server/plugins/PluginsLoader.js
+++ ilc/server/plugins/PluginsLoader.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fg = require('fast-glob');
 
 const { Environment } = require('../../common/Environment');
-const { manifest: serverPluginsManifest } = require('../../server.plugins.manifest');
+const { manifest: serverPluginsManifest } = require('../../../server.plugins.manifest');
 
 class PluginsLoader {
     load() {
@@ -13,7 +13,7 @@ class PluginsLoader {
 
         if (environment.isLegacyPluginsDiscoveryEnabled()) {
             const pluginPaths = fg.sync(['ilc-plugin-*/package.json', '@*/ilc-plugin-*/package.json'], {
-                cwd: path.resolve(__dirname, '../node_modules'),
+                cwd: path.resolve(__dirname, '../../../node_modules'),
                 absolute: true,
             });
 
