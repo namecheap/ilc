40a995efb6f28f274f6a99494a049f15f9b14382
Artyom Zakharchenko <artyom.zakharchenko@namecheap.com>
2023-11-21 14:42:00 +0000

    fix: import es module in an appropriate way

--- ilc/client/registry/factory.js
+++ ilc/client/registry/factory.js
@@ -1,5 +1,5 @@
 import Registry from './Registry';
 import wrapWithCache from '../../common/wrapWithCache';
-import * as localStorage from '../../common/localStorage';
+import localStorage from '../../common/localStorage';
 
 export default new Registry(wrapWithCache(localStorage, console));
