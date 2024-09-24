b3cdea0c4f6f062cb31d5f2860a6c7252de8ae23
Mark <markh@sweepbright.com>
2024-09-24 12:16:18 +0200

    feat(registry/ui): Add ListFilter to apps page
    Also, add domainId filter parameter to /api/v1/app
    route.

---
+++ registry/client/src/apps/ListFilter.js
@@ -0,0 +1,42 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+import { Filter, TextInput, SelectInput } from 'react-admin';
+import { APP_KINDS_WITH_WRAPPER } from '../constants';
+
+const useStyles = makeStyles({
+    filters: {
+        alignItems: 'center',
+        marginTop: '0',
+    },
+    filtersSpecial: {
+        marginBottom: '-6px',
+    },
+});
+
+export const ListFilter = ({ routerDomain, ...props }) => {
+    const classes = useStyles();
+
+    return (
+        <Filter {...props} className={classes.filters}>
+            <TextInput label="Name" source="q" alwaysOn resettable />
+            <SelectInput
+                alwaysOn
+                source="kind"
+                label="Kind"
+                optionText="name"
+                resettable
+                choices={APP_KINDS_WITH_WRAPPER}
+            />
+            {routerDomain.length ? (
+                <SelectInput
+                    alwaysOn
+                    source="domainId"
+                    label="Domain"
+                    optionText="domainName"
+                    resettable
+                    choices={routerDomain}
+                />
+            ) : null}
+        </Filter>
+    );
+};
--- registry/server/apps/routes/getApps.ts
+++ registry/server/apps/routes/getApps.ts
@@ -1,19 +1,35 @@
-import { Request, Response } from 'express';
-
-import db from '../../db';
+import { Response } from 'express';
+import Joi from 'joi';
 import preProcessResponse from '../../common/services/preProcessResponse';
-import App, { appNameSchema } from '../interfaces';
+import db from '../../db';
 import { Tables } from '../../db/structure';
 import { appendDigest } from '../../util/hmac';
 import { EntityTypes } from '../../versioning/interfaces';
+import App from '../interfaces';
+import { RequestWithFilters, filtersMiddleware } from '../../middleware/filters';
 
-const getApps = async (req: Request, res: Response): Promise<void> => {
-    const filters = req.query.filter ? JSON.parse(req.query.filter as string) : {};
+interface Filters {
+    q?: string;
+    kind?: string | string[];
+    name?: string[];
+    id?: string[];
+    domainId?: number | 'null';
+}
+
+const filtersSchema = Joi.object<Filters>({
+    q: Joi.string().optional(),
+    kind: Joi.alternatives(Joi.string(), Joi.array().items(Joi.string())),
+    name: Joi.array().items(Joi.string()),
+    id: Joi.array().items(Joi.string()),
+    domainId: Joi.alternatives(Joi.number(), Joi.string().valid('null')),
+});
+
+const getApps = async (req: RequestWithFilters<Filters>, res: Response): Promise<void> => {
+    const filters = req.filters ?? {};
 
     const query = db.selectVersionedRowsFrom<App>(Tables.Apps, 'name', EntityTypes.apps, [`${Tables.Apps}.*`]);
-
     if (filters.id || filters.name) {
-        query.whereIn('name', [...(filters.id || filters.name)]);
+        query.whereIn('name', [...(filters.id ?? filters.name ?? [])]);
     }
     if (typeof filters.kind === 'string') {
         query.where('kind', filters.kind);
@@ -22,6 +38,24 @@ const getApps = async (req: Request, res: Response): Promise<void> => {
     }
     if (filters.q) {
         query.where('name', 'like', `%${filters.q}%`);
+    }
+    if (filters.domainId) {
+        if (filters.domainId === 'null') {
+            query.whereNotExists(function () {
+                this.select(1)
+                    .from(Tables.RouteSlots)
+                    .innerJoin(Tables.Routes, `${Tables.Routes}.id`, `${Tables.RouteSlots}.routeId`)
+                    .where(`${Tables.RouteSlots}.appName`, db.ref(`${Tables.Apps}.name`))
+                    .whereNotNull(`${Tables.Routes}.domainId`);
+            });
+        } else {
+            query
+                .innerJoin(Tables.RouteSlots, `${Tables.RouteSlots}.appName`, `${Tables.Apps}.name`)
+                .innerJoin(Tables.Routes, `${Tables.Routes}.id`, `${Tables.RouteSlots}.routeId`)
+                .where(`${Tables.Routes}.domainId`, filters.domainId)
+                .groupBy(`${Tables.Apps}.name`)
+                .groupBy('v.versionId');
+        }
     }
 
     const apps = await query.range(req.query.range as string | undefined);
@@ -33,4 +67,4 @@ const getApps = async (req: Request, res: Response): Promise<void> => {
     res.status(200).send(preProcessResponse(itemsWithId));
 };
 
-export default [getApps];
+export default [filtersMiddleware(filtersSchema), getApps];
