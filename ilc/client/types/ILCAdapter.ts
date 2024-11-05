f61a1b0612d25f3dbd1e2ba59758437c782cba6c
Volodymyr Malyhin <volodymyr.malygin@namecheap.com>
2024-11-05 10:51:32 +0300

    chore(ilc): type fixing

--- ilc/client/types/ILCAdapter.ts
+++ ilc/client/types/ILCAdapter.ts
@@ -3,7 +3,7 @@ import { AppConfig } from './AppConfig';
 
 export type CreateNewReturnType = Promise<ILCAdapter> | undefined | any;
 
-export type CreateNewArgs = [{ appConfig?: AppConfig }, ...any[]];
+export type CreateNewArgs = [{ appConfig?: AppConfig } & any];
 
 export type ILCAdapter = LifeCycles<any> & {
     createNew?: (...args: CreateNewArgs) => CreateNewReturnType;
