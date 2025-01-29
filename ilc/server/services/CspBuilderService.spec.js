bdd29bfa48d8c827b38283f2cf9f48a6ca94d061
Stanislav Mishchyshyn <stanislav.mishchyshyn@namecheap.com>
2025-01-29 13:58:28 +0200

    feat(ilc-server): support more csp directives

--- ilc/server/services/CspBuilderService.spec.js
+++ ilc/server/services/CspBuilderService.spec.js
@@ -5,8 +5,25 @@ const CspBuilderService = require('./CspBuilderService');
 
 const cspConfigFull = {
     defaultSrc: ['https://test.com'],
+    connectSrc: ['https://connect.com'],
     scriptSrc: ['https://script.com'],
+    styleSrc: ['https://style.com'],
+    fontSrc: ['https://font.com'],
+    imgSrc: ['https://img.com'],
+    workerSrc: ['https://worker.com'],
+    frameSrc: ['https://frame.com'],
     reportUri: '/a/b',
+    mediaSrc: ['https://media.com'],
+    childSrc: ['https://child.com'],
+    formAction: ['https://form.com'],
+    manifestSrc: ['https://manifest.com'],
+    objectSrc: ['https://object.com'],
+    scriptSrcAttr: ['https://scriptattr.com'],
+    scriptSrcElem: ['https://scriptelem.com'],
+    baseUri: ['https://base.com'],
+    frameAncestors: ['https://ancestors.com'],
+    sandbox: ['allow-scripts'],
+    upgradeInsecureRequests: true,
 };
 
 describe('CSP builder', () => {
@@ -23,7 +40,7 @@ describe('CSP builder', () => {
         chai.expect(res.setHeader.calledOnce).to.be.true;
         chai.expect(res.setHeader.getCall(0).args[0]).to.be.eq('content-security-policy-report-only');
         chai.expect(res.setHeader.getCall(0).args[1]).to.be.eq(
-            'default-src https://test.com; script-src https://script.com; report-uri /a/b',
+            'default-src https://test.com; connect-src https://connect.com; script-src https://script.com; style-src https://style.com; font-src https://font.com; img-src https://img.com; worker-src https://worker.com; frame-src https://frame.com; media-src https://media.com; child-src https://child.com; form-action https://form.com; manifest-src https://manifest.com; object-src https://object.com; script-src-attr https://scriptattr.com; script-src-elem https://scriptelem.com; base-uri https://base.com; frame-ancestors https://ancestors.com; sandbox allow-scripts; upgrade-insecure-requests; report-uri /a/b',
         );
     });
 
@@ -40,7 +57,7 @@ describe('CSP builder', () => {
         chai.expect(res.setHeader.calledOnce).to.be.true;
         chai.expect(res.setHeader.getCall(0).args[0]).to.be.eq('content-security-policy');
         chai.expect(res.setHeader.getCall(0).args[1]).to.be.eq(
-            'default-src https://test.com; script-src https://script.com; report-uri /a/b',
+            'default-src https://test.com; connect-src https://connect.com; script-src https://script.com; style-src https://style.com; font-src https://font.com; img-src https://img.com; worker-src https://worker.com; frame-src https://frame.com; media-src https://media.com; child-src https://child.com; form-action https://form.com; manifest-src https://manifest.com; object-src https://object.com; script-src-attr https://scriptattr.com; script-src-elem https://scriptelem.com; base-uri https://base.com; frame-ancestors https://ancestors.com; sandbox allow-scripts; upgrade-insecure-requests; report-uri /a/b',
         );
     });
 
@@ -70,7 +87,7 @@ describe('CSP builder', () => {
         chai.expect(res.setHeader.calledOnce).to.be.true;
         chai.expect(res.setHeader.getCall(0).args[0]).to.be.eq('content-security-policy');
         chai.expect(res.setHeader.getCall(0).args[1]).to.be.eq(
-            'default-src https://test.com https://localhost:* b; script-src https://script.com https://localhost:* b; report-uri /a/b',
+            'default-src https://test.com https://localhost:* b; connect-src https://connect.com https://localhost:* b; script-src https://script.com https://localhost:* b; style-src https://style.com https://localhost:* b; font-src https://font.com https://localhost:* b; img-src https://img.com https://localhost:* b; worker-src https://worker.com https://localhost:* b; frame-src https://frame.com https://localhost:* b; media-src https://media.com https://localhost:* b; child-src https://child.com https://localhost:* b; form-action https://form.com https://localhost:* b; manifest-src https://manifest.com https://localhost:* b; object-src https://object.com https://localhost:* b; script-src-attr https://scriptattr.com https://localhost:* b; script-src-elem https://scriptelem.com https://localhost:* b; base-uri https://base.com https://localhost:* b; frame-ancestors https://ancestors.com https://localhost:* b; sandbox allow-scripts https://localhost:* b; upgrade-insecure-requests; report-uri /a/b',
         );
     });
 });
