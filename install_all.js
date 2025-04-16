ae447661e8244ae43f3dd2af529ecb946dd4b9e7
Volodymyr Malyhin <volodymyr.malyhin@namecheap.com>
2025-04-16 18:21:05 +0300

    chore: update node parameters and test running

--- install_all.js
+++ install_all.js
@@ -1,18 +1,15 @@
 const spawnSync = require('child_process').spawnSync;
 
 const commands = [
-    { command: 'cd ./ilc/ && npm ci', name: 'ilc' },
-    { command: 'cd ./registry/ && npm ci', name: 'registry' },
-    { command: 'cd ./registry/client && npm ci', name: 'registry:ui' },
-    { command: 'cd ./e2e && npm ci', name: 'e2e' },
+    { command: 'cd ./ilc/ && npm i', name: 'ilc' },
+    { command: 'cd ./registry/ && npm i', name: 'registry' },
+    { command: 'cd ./registry/client && npm i', name: 'registry:ui' },
+    { command: 'cd ./e2e && npm i', name: 'e2e' },
 ];
 
 commands.forEach((cmd) => {
     console.log('');
     console.log('');
-
-    spawnSync('npm config set network-concurrency 4', { shell: true });
-
     console.log(`Installing packages for "${cmd.name}"... Calling: ${cmd.command}`);
     console.log('');
     const res = spawnSync(cmd.command, { shell: true, killSignal: 'SIGKILL', stdio: 'inherit' });
