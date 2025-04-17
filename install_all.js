7e2ec9e6d30c84edf52ae6568752fc270cb5a966
Volodymyr Malyhin <volodymyr.malyhin@namecheap.com>
2025-04-17 12:24:48 +0300

    feat: revert logging level

--- install_all.js
+++ install_all.js
@@ -1,10 +1,10 @@
 const spawnSync = require('child_process').spawnSync;
 
 const commands = [
-    { command: 'cd ./ilc/ && npm i --timing --loglevel verbose', name: 'ilc' },
-    { command: 'cd ./registry/ && npm i --timing --loglevel verbose', name: 'registry' },
-    { command: 'cd ./registry/client && npm i --timing --loglevel verbose', name: 'registry:ui' },
-    { command: 'cd ./e2e && npm i --timing --loglevel verbose', name: 'e2e' },
+    { command: 'cd ./ilc/ && npm i', name: 'ilc' },
+    { command: 'cd ./registry/ && npm i', name: 'registry' },
+    { command: 'cd ./registry/client && npm i', name: 'registry:ui' },
+    { command: 'cd ./e2e && npm i', name: 'e2e' },
 ];
 
 commands.forEach((cmd) => {
